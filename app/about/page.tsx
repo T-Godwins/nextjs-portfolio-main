e852822769d788ef304b395f4f1bf528f3ef6fc7
T-Godwins <gtuyishime@gmail.com>
2024-07-23 19:22:51 -0400

    first commit

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Navigation } from "../components/nav";
+import { Card } from "../components/card";
+
+export default function AboutMe(){
+    return(
+        <div className="bg-red">
+            <h1>About Me</h1>
+        </div>
+    );
+}
